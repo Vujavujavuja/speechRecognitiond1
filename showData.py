8d6af36e2ee19cd4f71ea76c3343d4d0493300a3
Nemanja Vujic <nvujic2002@gmail.com>
2024-11-06 18:00:17 +0100

    Testing results

--- showData.py
+++ showData.py
@@ -3,9 +3,11 @@ import numpy as np
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
+from scipy.signal import hilbert
+from scipy.signal import find_peaks
 
 # file pat to open
-fp = 'data/debussy.wav'
+fp = 'data/ted.wav'
 
 # signal and sample rate same as raw
 signal, sample_rate = librosa.load(fp, sr=None)
@@ -25,12 +27,17 @@ rms_time = [
     for i in range(0, len(signal), frame_size)
 ]
 
+analytic_signal = hilbert(signal)
+amplitude_envelope = np.abs(analytic_signal)
+
 plt.figure(figsize=(15, 4))
 
 # show bcs plot is deprecated
 librosa.display.waveshow(signal, sr=sample_rate, alpha=0.5)
 
 plt.plot(rms_time, rms, color='r', label='RMS', linewidth=0.5)
+peaks, _ = find_peaks(signal)
+valleys, _ = find_peaks(-signal)
 
 # min max from signal array
 max_amp = np.max(signal)
@@ -42,9 +49,27 @@ max_time = np.argmax(signal) / sample_rate
 min_time = np.argmin(signal) / sample_rate
 min_abs_time = np.argmin(np.abs(signal)) / sample_rate
 
+
+
+# plt.plot(peaks / sample_rate, signal[peaks], 'ro', label='Detected Peaks', markersize=5)
+# plt.plot(valleys / sample_rate, signal[valleys], 'yo', label='Detected Valleys', markersize=5)
+
 plt.plot(max_time, max_amp, 'go', label='Max', markersize=3)
 plt.plot(min_time, min_amp, 'bo', label='Min', markersize=3)
+
 # plt.plot(min_abs_time, min_abs_amp, 'ro', label='Min Abs')
+
+print('Max Amplitude:', max_amp)
+print('Max Time:', max_time)
+print('Max Index:', np.argmax(signal))
+
+start = max(0, np.argmax(signal) - frame_size)
+end = min(len(signal), np.argmax(signal) + frame_size)
+
+print("Maximum value in isolated window:", np.max(signal[start:end]))
+overall_max = np.max(signal)
+print("Overall Maximum Amplitude in Waveform:", overall_max)
+
 
 plt.title('Waveform')
 plt.xlabel('Time')
